c4559a8c33a150070fd74ac5e2dde39a2b832c4b
slhillk2 <stephenhill@stephens-MacBook-Pro.local>
2022-05-06 12:32:18 +0100

    alter header from h1 to logo image in the header component to make change for remaining html pages

--- components/header.js
+++ components/header.js
@@ -1,9 +1,9 @@
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <header class="head-logo">
-                <h1>Lookout <br> <span>on the pier</span> </h1>
-            </header>
+            <header class="head-banner">
+            <img src="img/lookout_logo_transbg2.png" alt="lookout on the pier logo image" class="logo">
+            </header> 
             `;
     }  
 }
