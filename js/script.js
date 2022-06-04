5017db8a743466459480c6f6457467d15c75cdfd
slhillk2 <stephenhill@stephens-MacBook-Pro.local>
2022-06-04 12:15:05 +0100

    add event listener script to burger menu, toggle active class on nav bar and toggle class on burger menu lines to make them cross

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const burgerMenu = document.querySelector('.burger-menu')
+const navMenu = document.querySelector('.nav-links')
+
+burgerMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active')
+    // burger animation
+    burgerMenu.classList.toggle('toggle')
+})
+
+
+
+
