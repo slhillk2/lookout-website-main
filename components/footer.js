9bc156e1318ad7c692fb9eea9e8befa185383bba
slhillk2 <stephenhill@stephens-MacBook-Pro.local>
2022-06-26 08:20:43 +0100

    add footer grid container to better structure the contact details and opening times grid items

--- components/footer.js
+++ components/footer.js
@@ -2,18 +2,20 @@ class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <footer>
-                <div class="contact-details">
-                    <div class="address">
-                        <i class="fa-solid fa-location-dot"></i>
-                        <a href="https://g.page/lookout-on-the-pier?share">West Pier, Scarborough, YO11 1PD</a>
-                    </div>
-                    <div class="email-address">
-                        <i class="fa-solid fa-envelope"></i>
-                        <a href="mailto:lookoutcafe8@gmail.com">lookoutcafe8@gmail.com</a>
-                    </div>
-                    <div class="number">
-                        <i class="fa-solid fa-phone"></i>
-                        <a href="tel:+01723351000">01723 351 000</a>
+                <div class="footer-grid-container">
+                    <div class="contact-details">
+                        <div class="address">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <a href="https://g.page/lookout-on-the-pier?share">West Pier, Scarborough, YO11 1PD</a>
+                        </div>
+                        <div class="email-address">
+                            <i class="fa-solid fa-envelope"></i>
+                            <a href="mailto:lookoutcafe8@gmail.com">lookoutcafe8@gmail.com</a>
+                        </div>
+                        <div class="number">
+                            <i class="fa-solid fa-phone"></i>
+                            <a href="tel:+01723351000">01723 351 000</a>
+                        </div>
                     </div>
                     <div class="opening-times">
                         <p>Mon - Fri:</p> <p>7:30am - 4pm</p>
